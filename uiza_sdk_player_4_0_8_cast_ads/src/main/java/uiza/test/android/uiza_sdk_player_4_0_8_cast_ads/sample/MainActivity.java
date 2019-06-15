06eaebca6129ebd650dd41e6b87202fe3deb5513
FRAMGIA\nguyen.thanh.minhb <nguyen.thanh.minhb@framgia.com>
2019-06-15 08:25:20 +0700

    uiza ver 4.0.8

---
+++ uiza_sdk_player_4_0_8_cast_ads/src/main/java/uiza/test/android/uiza_sdk_player_4_0_8_cast_ads/sample/MainActivity.java
@@ -0,0 +1,42 @@
+package uiza.test.android.uiza_sdk_player_4_0_8_cast_ads.sample;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import uiza.test.android.uiza_sdk_player_4_0_8_cast_ads.R;
+import uiza.test.android.uiza_sdk_player_4_0_8_cast_ads.app.LSApplication;
+import vn.uiza.core.common.Constants;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.bt_cast).setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, CustomSkinXMLCastActivity.class);
+            intent.putExtra(Constants.KEY_UIZA_ENTITY_ID, LSApplication.entityIdDefaultVOD);
+            startActivity(intent);
+        });
+
+        if (LSApplication.DF_DOMAIN_API.equals("input")) {
+            showDialogInitWorkspace();
+        }
+    }
+
+    private void showDialogInitWorkspace() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Please correct your workspace's information first..");
+        builder.setCancelable(false);
+        builder.setPositiveButton(
+                "Yes",
+                (dialog, id) -> {
+                    dialog.cancel();
+                    onBackPressed();
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+}
